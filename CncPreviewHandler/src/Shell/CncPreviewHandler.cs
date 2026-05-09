b6ce0b0f1da0f80aa5fb168e3ae3be24a48418ca
Thane H. Fitzgerald <tfitzgerald@rogers.com>
2026-05-08 23:16:20 -0400

    Fix: use reflection to read SharpShell internal file path field

--- CncPreviewHandler/src/Shell/CncPreviewHandler.cs
+++ CncPreviewHandler/src/Shell/CncPreviewHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
 using SharpShell.Attributes;
 using SharpShell.SharpPreviewHandler;
 
@@ -8,20 +9,27 @@ namespace CncPreviewHandler.Shell
     [Guid("B1C2D3E4-F5A6-7890-BCDE-F01234567891")]
     [COMServerAssociation(AssociationType.ClassOfExtension,
         ".nc", ".gcode", ".gc", ".g", ".tap", ".cnc")]
-    [PreviewHandler]
     [DisplayName("CNC Toolpath Preview Handler")]
-    public class CncPreviewHandlerServer : SharpPreviewHandler, IInitializeWithFile
+    [PreviewHandler]
+    public class CncPreviewHandlerServer : SharpPreviewHandler
     {
-        private string _filePath;
-
-        void IInitializeWithFile.Initialize(string pszFilePath, uint grfMode)
-        {
-            _filePath = pszFilePath;
-        }
-
         protected override PreviewHandlerControl DoPreview()
         {
-            return new CncPreviewControl(_filePath ?? string.Empty);
+            // SharpShell stores the path in a private field after IInitializeWithFile
+            // is called internally. Read it via reflection since it is not exposed.
+            string filePath = null;
+
+            foreach (var name in new[] { "filePath", "selectedItemPath",
+                                         "_filePath",  "m_filePath", "path" })
+            {
+                var field = typeof(SharpPreviewHandler).GetField(name,
+                    BindingFlags.NonPublic | BindingFlags.Instance);
+                if (field == null) continue;
+                filePath = field.GetValue(this) as string;
+                if (!string.IsNullOrEmpty(filePath)) break;
+            }
+
+            return new CncPreviewControl(filePath ?? string.Empty);
         }
     }
 }
